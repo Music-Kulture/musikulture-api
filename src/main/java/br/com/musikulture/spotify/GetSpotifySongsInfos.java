47bc7c7ed78f4af60540ac9266a73adf1143cf53
daniel <ddeniel1@yahoo.com.br>
2020-12-02 23:54:50 -0300

    hotfix -> CORS

--- src/main/java/br/com/musikulture/spotify/GetSpotifySongsInfos.java
+++ src/main/java/br/com/musikulture/spotify/GetSpotifySongsInfos.java
@@ -3,7 +3,10 @@ package br.com.musikulture.spotify;
 import br.com.musikulture.music.TrackAnalyzed;
 import br.com.musikulture.musixmatch.MusixMatchApiRequest;
 import com.wrapper.spotify.model_objects.specification.SavedTrack;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.message.HttpResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,15 +24,15 @@ public class GetSpotifySongsInfos {
     MusixMatchApiRequest musixMatchApiRequest;
 
     @GetMapping("/tracks")
-    public List<TrackAnalyzed> objects(@RequestParam(name = "token") String token,
-                                       @RequestParam(name = "lang", defaultValue = "en") String lang) {
+    public ResponseEntity<?> objects(@RequestParam(name = "token") String token,
+                                  @RequestParam(name = "lang", defaultValue = "en") String lang) {
         List<SavedTrack> savedTracks = webAPISpotifyRequest.getUsersSavedTracks_Sync(token);
 
         RecommendationBuilder recommendationBuilder = new RecommendationBuilder(
                 musixMatchApiRequest.analyze(Objects.requireNonNull(savedTracks), webAPISpotifyRequest));
 
 
-        return recommendationBuilder.getRecommendationListFromTrackAnalyzedList(lang, webAPISpotifyRequest, musixMatchApiRequest);
+        return ResponseEntity.ok(recommendationBuilder.getRecommendationListFromTrackAnalyzedList(lang, webAPISpotifyRequest, musixMatchApiRequest));
 
 
     }
